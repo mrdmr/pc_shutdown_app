5c6fcd5bec3cb3e6df49d7f43e5b17395386c6fa
mrdmr <34637640+mrdmr@users.noreply.github.com>
2017-12-24 01:12:57 +0300

    Add files via upload

---
+++ Computer_shutdown_app/Computer_shutdown_app/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Computer_shutdown_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //https://steemit.com/@sameddemir
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "C:\\Windows\\system32\\shutdown.exe";
+            psi.Arguments = "-f -s -t 0";
+            Process.Start(psi);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string hour, minute, second;
+            hour = textBox1.Text;
+            minute = textBox2.Text;
+            second = textBox3.Text;
+
+
+            if ((Convert.ToString(DateTime.Now.Hour)) == hour && (Convert.ToString(DateTime.Now.Minute) == minute) && (Convert.ToString(DateTime.Now.Second)) == second)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = "C:\\Windows\\system32\\shutdown.exe";
+                psi.Arguments = "-f -s -t 0";
+                Process.Start(psi);
+            }
+        }
+    }
+}
---
+++ Computer_shutdown_app/Computer_shutdown_app/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Computer_shutdown_app")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Computer_shutdown_app")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("25a0564f-ff30-4dd6-a2da-5327cf593d7b")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// '*' kullanarak varsayılana ayarlayabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
